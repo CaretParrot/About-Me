167066c2321a9d4d5ac36cced5e69945c5d9664c
CaretParrot <wopow29@gmail.com>
2024-07-06 15:41:25 -0500

    Added project library to about me page

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let currentPageId = "aboutMe";
 
+function switchPage(pageId) {
+    let allPages = document.getElementsByClassName("page");
+    for (let i = 0; i < allPages.length; i++) {
+        allPages[i].style.display = "none";
+    }
+
+    let allNavButtons = document.getElementsByClassName("navButtons");
+    for (let i = 0; i < allNavButtons.length; i++) {
+        allNavButtons[i].style.backgroundColor = "hsl(240, 25%, 25%)";
+    }
+    document.getElementById(pageId + "Button").style.backgroundColor = "hsl(240, 30%, 30%)";
+    document.getElementById(pageId).style.display = "flex";
+}
