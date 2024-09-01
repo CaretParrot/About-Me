5b11dce82a376b5bd577a36bee9e2a883b93fa62
CaretParrot <wopow29@gmail.com>
2024-09-01 12:00:22 -0500

    Fixing styles for about me page

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function switchPage(pageId) {
 
     let allNavButtons = document.getElementsByClassName("navButtons");
     for (let i = 0; i < allNavButtons.length; i++) {
-        allNavButtons[i].style.backgroundColor = "hsl(240, 20%, 20%)";
+        allNavButtons[i].style.backgroundColor = "hsl(240, 10%, 10%)";
     }
-    document.getElementById(pageId + "Button").style.backgroundColor = "hsl(240, 25%, 25%)";
+    document.getElementById(pageId + "Button").style.backgroundColor = "hsl(240, 20%, 20%)";
     document.getElementById(pageId).style.display = "flex";
 }
