5ea6bad6d1933724a7acae43ba0efea2585d3b77
CaretParrot <wopow29@gmail.com>
2024-10-16 18:26:15 -0500

    Added active class for light mode support

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function switchPage(pageId) {
 
     let allNavButtons = document.getElementsByClassName("navButtons");
     for (let i = 0; i < allNavButtons.length; i++) {
-        allNavButtons[i].style.backgroundColor = "hsl(240, 10%, 10%)";
+        allNavButtons[i].classList.remove("active");
     }
-    document.getElementById(pageId + "Button").style.backgroundColor = "hsl(240, 20%, 20%)";
+    document.getElementById(pageId + "Button").classList.add("active");
     document.getElementById(pageId).style.display = "flex";
 }
